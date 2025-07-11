8e7b8f65225d7f274590bbcfb7dc5047325ed649
devMRibeiro <michael.douglas2020@hotmail.com>
2025-07-11 17:12:05 -0300

    First commit

---
+++ src/test/java/com/github/devmribeiro/thinkfast/thinkfast/ThinkfastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.devmribeiro.thinkfast.thinkfast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinkfastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

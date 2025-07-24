84e07d2ce75de380fb9211f0065c696b37646399
devMRibeiro <michael.douglas2020@hotmail.com>
2025-07-24 00:54:17 -0300

    fix: rename method

--- src/main/java/com/github/devmribeiro/thinkfast/repository/QuizRepository.java
+++ src/main/java/com/github/devmribeiro/thinkfast/repository/QuizRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.github.devmribeiro.thinkfast.model.Quiz;
 
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
-	Quiz findQuizById(Long quizId);
+	Quiz findQuizByQuizId(Long quizId);
 }
